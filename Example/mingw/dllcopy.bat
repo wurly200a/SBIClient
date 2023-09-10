a01977e757c060389a5d09940b30a19b7db15d2a
wurly200a <>
2023-09-10 23:00:01 +0900

    Added files related to mingw build

---
+++ Example/mingw/dllcopy.bat
@@ -0,0 +1,4 @@
+copy ..\..\mingw\sbiclient.dll .\
+copy "C:\msys64\mingw32\bin\libgcc_s_dw2-1.dll"  .\
+copy "C:\msys64\mingw32\bin\libstdc++-6.dll"     .\
+copy "C:\msys64\mingw32\bin\libwinpthread-1.dll" .\
